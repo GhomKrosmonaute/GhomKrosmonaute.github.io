b47ca86f81a20ca04039cc5df7f74be47d00c111
GhomKrosmonaute <camille.abella@hotmail.fr>
2024-08-13 16:36:13 +0200

    make persistant the modal state

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -4,6 +4,12 @@ export const useModal = create<{
   modal: false | "contact" | "tarifs";
   setModal: (modal: false | "contact" | "tarifs") => void;
 }>((set) => ({
-  modal: false,
-  setModal: (modal: false | "contact" | "tarifs") => set({ modal }),
+  modal:
+    (localStorage.getItem("modal") as false | "contact" | "tarifs") || false,
+  setModal: (modal: false | "contact" | "tarifs") => {
+    set({ modal });
+
+    if (modal) localStorage.setItem("modal", modal);
+    else localStorage.removeItem("modal");
+  },
 }));
