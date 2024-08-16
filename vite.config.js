57a91b14e89287a9e92fa2ca76808b49cb1cf430
GhomKrosmonaute <camille.abella@hotmail.fr>
2024-08-16 10:12:44 +0200

    removed user-select on game cards & replaced vite.config.ts by a js file

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), visualizer()],
+  base: "/portfolio-libless/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("@splinetool/react-spline")) {
+              return "react-spline";
+            }
+          }
+        },
+      },
+    },
+  },
+});
