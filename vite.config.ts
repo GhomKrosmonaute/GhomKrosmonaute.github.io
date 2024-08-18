63ba22be40608d2e93d121fb3d943065ede53826
GhomKrosmonaute <camille.abella@hotmail.fr>
2024-08-18 14:47:20 +0200

    fixed deployment as main portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer()],
-  base: "/portfolio-libless/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
