2794b304c4c1d53530b6148848f9249afa8bb0a6
GhomKrosmonaute <camille.abella@hotmail.fr>
2024-08-25 17:21:08 +0200

    added version management on deploy

---
+++ metadata.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+
+const file = fs.readFileSync("src/game-metadata.ts", "utf8");
+
+const version = file.match(/version: "(.*)"/)[1];
+
+const split = version.split(".");
+
+const newVersion = `${split[0]}.${split[1]}.${parseInt(split[2]) + 1}`;
+
+const newFile = file.replace(version, newVersion);
+
+fs.writeFileSync("src/game-metadata.ts", newFile);
