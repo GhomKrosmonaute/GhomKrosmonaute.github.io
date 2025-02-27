97f11e9b0ebcc10f7c9c75c11d0b23f1acc846c0
Ghom <camille.abella@hotmail.fr>
2025-02-27 13:39:06 +0100

    added react-scan from npm and in index.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { scan } from "react-scan"
 import App from "./App.tsx"
 import "./styles/index.scss"
+
+scan({
+  enabled: import.meta.env.DEV,
+})
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
